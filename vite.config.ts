294e3e482bfbcff7487f72a403c4078e9058b1cb
sudoapt-ux <vulimwabravin9@gmail.com>
2025-07-03 19:54:48 +0300

    Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/react/', // Set base path for deployment in /react subdirectory
   server: {
     host: "::",
     port: 8080,
