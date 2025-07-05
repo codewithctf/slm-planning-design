25eae144726f83f3365beae64db1d2700771a127
sudoapt-ux <vulimwabravin9@gmail.com>
2025-07-05 16:21:30 +0300

    Routing

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './', // Ensures relative paths work in static hosting
+  base: '/', // Ensures relative paths work in static hosting
   build: {
     assetsDir: 'assets', //  Puts JS/CSS/etc. inside /Assets instead of default /assets
   },
